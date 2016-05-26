597a0eadac57563f31279a1525e7cdc0decf91eb
Tim Hoolihan <tim@hoolihan.net>
2016-05-25 20:38:12 -0400

    color change

--- R/explore.R
+++ R/explore.R
@@ -6,6 +6,6 @@ data.train <- read.csv('data/train.csv',
                        colClasses = col.types)
 
 g <- ggplot(data = data.train, aes(x = label)) +
-      geom_bar(fill = '#000044')
+      geom_bar(fill = '#33bbff')
 
 print(g)
