5908f37cd1de9b177b78fe5c8ba6fba62718b91b
Tim Hoolihan <tim@hoolihan.net>
2016-06-13 17:55:10 -0400

    fix bug

--- R/nn_deep.R
+++ R/nn_deep.R
@@ -13,8 +13,8 @@ data.test <- data.train[-tri,]
 data.train <- data.train[tri,]
 
 nToMatrix <- function(v) {
-  m <- matrix(0, nrow = nrow(v), ncol = length(unique(v)))
-  for(i in 1:nrow(v)) { m[i, v[i]] = 1 }
+  m <- matrix(0, nrow = length(v), ncol = length(unique(v)))
+  for(i in 1:length(v)) { m[i, v[i]] = 1 }
   m
 }
 
@@ -23,8 +23,7 @@ y <- nToMatrix(data.train[, 1])
 
 print('training model...')
 
-model <- nnet(
-              x = X,
+model <- nnet(x = X,
               y = y,
               data = data.train,
               decay = 4.75,
@@ -34,7 +33,7 @@ model <- nnet(
 
 print('applying model...')
 XT <- data.matrix(data.test[, -1])
-data.test$output <- predict(model, XT)
+data.test$output <- max.col(predict(model, XT))
 
 print('summarizing results...')
 results <- caret::confusionMatrix(data.test$output, data.test$label)
