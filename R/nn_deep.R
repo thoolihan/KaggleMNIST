34255e6f643a2bfb11e13032f31d978244aa1097
Tim Hoolihan <tim@hoolihan.net>
2016-06-13 17:34:31 -0400

    Changing response to matrix

--- R/nn_deep.R
+++ R/nn_deep.R
@@ -12,18 +12,25 @@ tri <- createDataPartition(data.train$label, p = .75, list = FALSE)
 data.test <- data.train[-tri,]
 data.train <- data.train[tri,]
 
+nToMatrix <- function(v) {
+  m <- matrix(0, nrow = nrow(v), ncol = length(unique(v)))
+  for(i in 1:nrow(v)) { m[i, v[i]] = 1 }
+  m
+}
+
 X <- data.matrix(data.train[, -1])
-y <- data.train[, 1]
+y <- nToMatrix(data.train[, 1])
 
 print('training model...')
 
-model <- multinom(
+model <- nnet(
               x = X,
               y = y,
               data = data.train,
               decay = 4.75,
               size = 4,
-              MaxNWts = 5000)
+              MaxNWts = 5000,
+              softmax = TRUE)
 
 print('applying model...')
 XT <- data.matrix(data.test[, -1])
