09f8d6020aa03423cb5f5e12540536ecafa79abe
Tim Hoolihan <tim@hoolihan.net>
2016-06-13 17:54:44 -0400

    Add some output to allow user to know what happened with loading

--- R/load.R
+++ R/load.R
@@ -3,10 +3,15 @@
 if(!exists('data.raw')) { 
   if(file.exists(".RData")) {
     load(".RData") 
+    print('loaded data.raw from .RData')
   } else {
     n_pixels <- 784
     col.types <- c('integer', rep('integer', n_pixels))
     data.raw <- read.csv('data/train.csv', colClasses = col.types)
+    print('read data.raw from csv')
     save(data.raw, file = ".RData", compress = TRUE)
+    print('saved data.raw to .RData')
   }
+} else {
+  print('data.raw already exists')
 }
